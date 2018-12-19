bd8c823ad7992276f4dfbdfecbd53367277e9866
OoiBP <bpooi0702@gmail.com>
2018-12-19 17:22:31 +0800

    Add MDN projects

---
+++ MDN/first/scripts/main.js
@@ -0,0 +1,41 @@
+/* Comment */
+// Comment also
+
+/*
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+*/
+
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
